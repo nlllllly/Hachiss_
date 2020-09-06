1bbd90a8440b22b6c0203b79570f7d54cded8254
vagrant <vagrant@localhost.localdomain>
2020-09-06 01:35:43 +0000

    admins/index作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'home/index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
