0650fe1f2d356f19b02903310624dceba31010d2
vagrant <vagrant@localhost.localdomain>
2020-09-08 09:28:13 +0000

    配送先コントローラのみ

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,2 +1,57 @@
 class ShippingsController < ApplicationController
+    before_action :authenticate_customer!
+    before_action :set_shipping, only: [:edit, :update, :destroy]
+
+    def index
+        @shippings = Shipping.page(params[:page]).per(20)
+        @shipping = Shipping.new
+    end
+
+    def create
+        @shipping = Shipping.new(shipping_params)
+        if @shipping.save
+            redirect_to shippings_path
+        else
+            render :index, alert: "保存できませんでした"
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @shipping.update(shipping_params)
+            redirect_to shippings_path
+        else
+            render :edit, alert: "保存できませんでした"
+        end
+    end
+
+    def destroy
+    end
+    
+
+
+    private
+    def shipping_params
+        params.require(:shipping).permit(
+            :last_name_kana,
+            :first_name_kana,
+            :last_name, :first_name,
+            :postcode,
+            :prefecture_code,
+            :prefecture_name,
+            :address_city,
+            :address_street,
+            :address_building
+            )
+        end
+
+
+    def set_shipping
+        @shipping = Shipping.find(params[:id])
+    end
+
+
+
 end
