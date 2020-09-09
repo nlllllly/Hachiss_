301ae8a4c3bc94226ba9b272d45b68ead00921dd
vagrant <vagrant@localhost.localdomain>
2020-09-09 07:35:53 +0000

    会員genre修正

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,11 +1,17 @@
 class GenresController < ApplicationController
     
-    def show
-        @genres = Genre.where(genre_status: :active)
-        @genre = Genre.find(params[:id])
-        @products = @genre.products.where(genre_status: :active).page(params[:page]).per(8)
-        @product = @genre.products.where(genre_status: :active)
-    end
+    before_action :set_genre, only: [:show]
 
+    def show
+        # ジャンルidに所属し、かつ「販売中」か「売り切れ」の商品を表示する
+        @products = @genre.products.where(genre_id: @genre.id, sale_status: 1 || 2).page(params[:page]).per(20)
+    end
+    
+    
+    
+    private
+    def set_genre
+        @genre = Genre.find(params[:id])
+    end
 
 end
