a6a0d21110a6436b2453c00c47ded8a91d09adc4
vagrant <vagrant@localhost.localdomain>
2020-09-14 07:05:51 +0000

    管理者ページ修正

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -3,12 +3,33 @@ class Admins::OrderDetailsController < ApplicationController
     before_action :set_order_detail, only: [:update]
 
     def update
+        if @order_detail.update(order_detail_params)
+            # 商品ステータスが全て「2(準備完了)」になったら注文ステータスも「2(発送準備中)」にする。
+            if @order_detail.order.order_details.all?{|order_detail| order_detail.product_status == "ready"}
+                @order = Order.find(@order_detail.order_id)
+                @order.update(order_status: "preparing")
+                @order_detail.update(order_detail_params)
+                redirect_to request.referer, notice: "商品ステータスを更新しました"
+            else
+                @order_detail.update(order_detail_params)
+                redirect_to request.referer, notice: "商品ステータスを更新しました"
+            end
+        else
+            @order = Order.find(params[:id])
+            @customer = @order.customer_id
+            @order_details = OrderDetail.where(order_id: @order.id)
+            render 'admins/orders/show', alert: "更新できませんでした"
+        end
     end
 
 
 
 
     private
+    def order_detail_params
+        params.require(:order_detail).permit(:product_status)
+    end
+
     def set_order_detail
         @order_detail = OrderDetail.find(params[:id])
     end
--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,14 +7,32 @@ class Admins::OrdersController < ApplicationController
     end
 
     def show
+        @customer = @order.customer_id
     end
 
     def update
+        if @order.update(order_params)
+            # 注文ステータスが「3（発送済み）」にしたら、商品ステータスを「2（準備完了）」にする
+            if @order.order_status == "shipped"
+                @order.order_details.update(product_status: "ready")
+                redirect_to request.referer, notice: "注文ステータスを更新しました"
+            else
+                redirect_to request.referer, notice: "注文ステータスを更新しました"
+            end
+        else
+            @customer = @order.customer_id
+            @order_details = OrderDetail.where(order_id: @order.id)
+            render :show, alert: "更新できませんでした"
+        end
     end
 
 
 
     private
+    def order_params
+        params.require(:order).permit(:order_status)
+    end
+
     def set_order
         @order = Order.find(params[:id])
     end
